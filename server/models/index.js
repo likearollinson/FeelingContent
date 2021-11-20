d9fbc2b861ca05a081281feb45ff1d06722a1626
Brad R <brad.w.rollinson@gmail.com>
2021-11-20 17:58:18 -0500

    update the rest of the models

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,4 @@
 const User = require("./User");
 const Episode = require("./Episode");
-const Podcast = require("./Podcast");
 const Comment = require("./Comment");
 module.exports = { User, Episode, Podcast, Comment };
